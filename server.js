998df7c893b9ac199b695a87458350be3adb6989
kuldeep raj gour <81853301+kuldeepgaur02@users.noreply.github.com>
2024-11-19 15:21:12 +0545

    Update server.js
    changes regarding port for vercel

--- server.js
+++ server.js
@@ -41,7 +41,8 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-const PORT = process.env.PORT ;
+const PORT = process.env.PORT || 3000;
+;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     console.log(`API Documentation available at http://localhost:${PORT}/api/docs`);
